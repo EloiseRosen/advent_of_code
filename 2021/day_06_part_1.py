0da8a680efc1afac186344963e32fbe05ac74a39
EloiseRosen <eloise.rosen@gmail.com>
2022-11-28 15:27:05 -0500

    day 6

---
+++ 2021/day_06_part_1.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+DAYS = 80
+lst = [int(row) for row in open('input.txt').read().split(',')]
+dct = Counter(lst)
+
+for day in range(0, DAYS):
+    for key in range(0, 9):
+        dct[key-1] = dct[key]
+    dct[8] = dct[-1]  # new babies
+    dct[6] += dct[-1]  # back to start of cycle
+    dct[-1] = 0
+
+ans = 0
+for count in dct.values():
+    ans += count
+print(ans)
