3b7a339470fc0756a03680c6bec804468e663491
EloiseRosen <eloise.rosen@gmail.com>
2022-12-03 18:56:05 -0500

    simplify

--- 2022/day_02_part_2.py
+++ 2022/day_02_part_2.py
@@ -2,29 +2,27 @@ lst = open('input.txt').read().split('\n')
 
 # Same indices are same move. If your move is 1 index ahead of their move, you win.
 their_moves = ['A', 'B', 'C']
-your_moves = ['X', 'Y', 'Z', 'X']  # extra entry for wrapping
-your_move_points = [1, 2, 3]
+your_move_points = [1, 2, 3, 1]  # extra entry for wrapping
 
 win_points = 6
 draw_points = 3
 
 
-def get_move(their_move, outcome):
+def get_move_points(their_move, outcome):
     their_idx = their_moves.index(their_move)
     if outcome == 'X':  # need to lose
         your_idx = their_idx-1 if their_idx-1 >= 0 else their_idx-1+3
-        return your_moves[your_idx]
+        return your_move_points[your_idx]
     elif outcome == 'Y':  # need to draw
-        return your_moves[their_moves.index(their_move)]
+        return your_move_points[their_moves.index(their_move)]
     elif outcome == 'Z':  # need to win
-        return your_moves[their_idx+1]
+        return your_move_points[their_idx+1]
 
 
 score = 0
 for line in lst:
     their_move, outcome = line.split(' ')
-    your_move = get_move(their_move, outcome)
-    score += your_move_points[your_moves.index(your_move)]
+    score += get_move_points(their_move, outcome)
     if outcome == 'Y':
         score += draw_points
     elif outcome == 'Z':
