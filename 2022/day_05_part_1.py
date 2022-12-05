e081c17858bd2cdedfe3d039a513b0c02f900813
EloiseRosen <eloise.rosen@gmail.com>
2022-12-05 12:53:27 -0500

    day 5

---
+++ 2022/day_05_part_1.py
@@ -0,0 +1,26 @@
+crates, moves = open('input.txt').read().split('\n\n')
+crates = crates.split('\n')[:-1]
+moves = moves.split('\n')
+num_crates = (len(crates[0])+1)//4
+
+stacks = [[] for _ in range(0, num_crates)]
+for crate_line in crates:
+    for crate_line_idx in range(1, len(crates[0]), 4):
+        box = crate_line[crate_line_idx]
+        if box != ' ':
+            stack_idx = (crate_line_idx-1)//4
+            stacks[stack_idx].insert(0, box)
+
+for move in moves:
+    move_lst = move.split(' ')
+    num_items_to_move = int(move_lst[1])
+    start = int(move_lst[3])
+    end = int(move_lst[5])
+    for _ in range(0, num_items_to_move):
+        box = stacks[start-1].pop()
+        stacks[end-1].append(box)
+
+tops = []
+for stack in stacks:
+    tops.append(stack[-1])
+print(''.join(tops))
