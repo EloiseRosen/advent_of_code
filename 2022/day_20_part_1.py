f450668714a07186eceb75d7c790f6f3842c0c62
EloiseRosen <eloise.rosen@gmail.com>
2022-12-24 11:23:40 -0500

    day 20

---
+++ 2022/day_20_part_1.py
@@ -0,0 +1,24 @@
+
+lst = [(idx, int(row)) for idx, row in enumerate(open('input.txt').read().split('\n'))]  # (item_id, val)
+
+for item_id in range(0, len(lst)):
+    # find the item we need to move this time
+    for idx in range(0, len(lst)):
+        tup = lst[idx]
+        if tup[0] == item_id:
+            break
+
+    # put it in its new position
+    if tup[1] != 0:
+        lst.pop(idx)
+        new_idx = idx + tup[1]
+        new_idx = new_idx % len(lst)
+        if new_idx < 0:
+            new_idx += len(lst)
+        lst.insert(new_idx, tup)
+        
+for idx, tup in enumerate(lst):
+    if tup[1] == 0:
+        zero_idx = idx
+        break
+print(lst[(zero_idx+1000)%len(lst)][1] + lst[(zero_idx+2000)%len(lst)][1] + lst[(zero_idx+3000)%len(lst)][1])
