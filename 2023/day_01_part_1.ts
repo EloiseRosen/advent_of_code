66aac5662d6f0e328f68c1891e8db7412d63938f
EloiseRosen <eloise.rosen@gmail.com>
2023-12-01 10:15:05 -0500

    day 1 part 1

---
+++ 2023/day_01_part_1.ts
@@ -0,0 +1,24 @@
+import fs from 'fs';
+
+const arr: string[] = fs.readFileSync('input.txt', 'utf8').split('\n');
+
+let rtn = 0;
+const regex = /[0-9]/;
+for (const s of arr) {
+  let first = '', last = '';
+  for (let i = 0; i < s.length; i++) {
+    if (regex.test(s[i])) {
+      first = s[i];
+      break;
+    }
+  }
+  for (let i = s.length - 1; i > -1; i--) {
+    if (regex.test(s[i])) {
+      last = s[i];
+      break;
+    }
+  }
+  rtn += Number(first + last);
+}
+
+console.log(rtn);
