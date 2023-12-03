f3088bce5fc2314b4f498a5a09020095faaca914
EloiseRosen <eloise.rosen@gmail.com>
2023-12-03 11:57:24 -0500

    day 3

---
+++ 2023/day_03_part_1.ts
@@ -0,0 +1,47 @@
+import fs from 'fs';
+
+
+const DIRS: [number, number][] = [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [1, 1], [-1, 1], [1, -1]];
+
+/**
+ * Checks if there's a non-digit, non-period character adjacent to the given grid cell,
+ * where "adjacent" is specified by dirs.
+ */
+function checkForSymbol(grid: string[], r: number, c: number, dirs: [number, number][]): boolean {
+  for (const [rChange, cChange] of dirs) {
+    const newR = r + rChange;
+    const newC = c + cChange;
+    if (newR < grid.length && newR >= 0 && newC < grid[0].length && newC >= 0 && 
+      /[.\d]/.test(grid[newR][newC]) === false) {
+        return true;
+    }
+  }
+  return false;
+}
+
+
+const grid: string[] = fs.readFileSync('input.txt', 'utf8').split('\n');
+let rtn = 0;
+for (let r = 0; r < grid.length; r++) {
+  let c = 0;
+  while (c < grid[0].length) {
+    if (/\d/.test(grid[r][c])) { // found a digit
+      let keep = checkForSymbol(grid, r, c, DIRS);
+      let c2 = c;
+      while (c2 + 1 < grid[0].length && /\d/.test(grid[r][c2+1])) { // find the rest of this number
+        c2 = c2 + 1;
+        if (!keep) { // if we don't have a reason to keep this number yet, check for new reasons
+          keep = checkForSymbol(grid, r, c2, DIRS);
+        }
+      }
+      if (keep) {
+        const fullNum = Number(grid[r].slice(c, c2+1));
+        rtn += fullNum;
+      }
+      c = c2 + 1; // we can advance until after the number we've found
+    } else {
+      c++;
+    }
+  }
+}
+console.log(rtn);
---
+++ 2023/day_03_part_2.ts
@@ -0,0 +1,54 @@
+import fs from 'fs';
+
+
+/**
+ * Retrieve the number that starts at the given grid position (grid[r][c]). Number can be 
+ * retrieved going left or right as specified by goLeft.
+ */
+function getLeftOrRightNum(grid: string[], r: number, c: number, goLeft: boolean): string {
+  const change = goLeft ? -1 : 1;
+  let c2 = c;
+  while (c2+change >= 0 && c2+change < grid[0].length && /\d/.test(grid[r][c2+change])) {
+    c2 = c2 + change;
+  }
+  return c2 > c ? grid[r].slice(c, c2+1) : grid[r].slice(c2, c+1);
+}
+
+
+const grid: string[] = fs.readFileSync('input.txt', 'utf8').split('\n');
+let rtn = 0;
+for (let r = 0; r < grid.length; r++) {
+  for (let c = 0; c < grid[0].length; c++) {
+    if (grid[r][c] === '*') {
+      const nums: number[] = [];
+
+      // left and right
+      for (const cChange of [-1, 1]) {
+        if (c+cChange >= 0 && c+cChange < grid[0].length && /\d/.test(grid[r][c+cChange])) {
+          nums.push(Number(getLeftOrRightNum(grid, r, c+cChange, cChange===-1)));
+        }
+      }
+
+      // up and down, including one over to catch diagonals
+      for (const rChange of [-1, 1]) {
+        // if there's a number in the middle, check to left and right and combine without double-counting middle digit
+        if (r+rChange >= 0 && r+rChange < grid.length && /\d/.test(grid[r+rChange][c])) {
+          const left = getLeftOrRightNum(grid, r+rChange, c, true);
+          const right = getLeftOrRightNum(grid, r+rChange, c, false);
+          nums.push(Number(left + right.slice(1)));
+        } else { // no number in middle, so just check left and right
+          for (const cChange of [-1, 1]) {
+            if (r+rChange >= 0 && r+rChange < grid.length && c+cChange >= 0 && c+cChange < grid[0].length 
+              && /\d/.test(grid[r+rChange][c+cChange])) {
+              nums.push(Number(getLeftOrRightNum(grid, r+rChange, c+cChange, cChange===-1)));
+            }
+          }
+        }
+      }
+      if (nums.length >= 2) {
+        rtn += nums.reduce((acc, val) => acc * val, 1);
+      }
+    }
+  }
+}
+console.log(rtn);
