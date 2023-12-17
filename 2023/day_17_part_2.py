9455e112febb450087099f4ae59c262a494f786c
EloiseRosen <eloise.rosen@gmail.com>
2023-12-17 11:37:48 -0500

    day 17

---
+++ 2023/day_17_part_2.py
@@ -0,0 +1,38 @@
+import heapq
+
+
+grid = [list(map(int, r)) for r in open('input.txt').read().split('\n')]
+DIRS =  [(0, 1), (0, -1), (1, 0), (-1, 0)]
+START = (0, 0)
+END = (len(grid)-1, len(grid[0])-1)
+MAX_STEPS_IN_DIR = 10
+MIN_STEPS_IN_DIR = 4
+
+seen = set()
+# "Because you already start in the top-left block, you don't incur that block's heat 
+# loss unless you leave that block and then return to it."
+min_heap = [(0, 0,0, 0,0, 0)] # cost, r,c, last_r_change,last_c_change, consecutive_steps_in_dir
+while min_heap:
+    cost, r,c, last_r_change,last_c_change, steps_in_dir = heapq.heappop(min_heap)
+    if (r,c, last_r_change,last_c_change, steps_in_dir) not in seen:
+        seen.add((r,c, last_r_change,last_c_change, steps_in_dir))
+
+        if (r, c) == END and steps_in_dir >= MIN_STEPS_IN_DIR:
+            print(cost)
+            break
+
+        for r_change, c_change in DIRS:
+            new_r = r + r_change
+            new_c = c + c_change
+            if (new_r >= 0 and new_r < len(grid) and new_c >= 0 and new_c < len(grid[0]) and  # in bounds
+                (last_r_change*-1, last_c_change*-1) != (r_change, c_change) and  # can't reverse direction
+                (steps_in_dir < MAX_STEPS_IN_DIR or (last_r_change, last_c_change) != (r_change, c_change)) and # can't move in same dir more than specified times
+                (steps_in_dir >= MIN_STEPS_IN_DIR or (r_change, c_change) == (last_r_change, last_c_change) or (last_r_change, last_c_change) == (0, 0))): # need to meet minimum steps in dir
+                    new_cost = cost + grid[new_r][new_c]
+
+                    if last_r_change == r_change and last_c_change == c_change:
+                        new_steps_in_dir = steps_in_dir+1
+                    else:
+                        new_steps_in_dir = 1
+
+                    heapq.heappush(min_heap, (new_cost, new_r,new_c, r_change,c_change, new_steps_in_dir))
